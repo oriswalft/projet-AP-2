8c41aba831c6e46b426fed8cd186ef1b6c0e20d8
Thomas Dindin <thomas.dindin@gmail.com>
2023-01-20 16:16:41 +0100

    Le mot de passe est désormais caché

--- projet_ap_javafx/src/main/java/com/example/PrimaryController.java
+++ projet_ap_javafx/src/main/java/com/example/PrimaryController.java
@@ -1,10 +1,7 @@
 package com.example;
 
 import java.io.IOException;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.SQLException;
+
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
