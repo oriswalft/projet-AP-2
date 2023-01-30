b0fae70cb8330d133700b0b8f7cf755c9d2faf42
Thomas Dindin <thomas.dindin@gmail.com>
2023-01-30 16:54:52 +0100

    Debut fdf et consultation

--- projet_ap_javafx/src/main/java/com/example/SecondaryController.java
+++ projet_ap_javafx/src/main/java/com/example/SecondaryController.java
@@ -1,12 +1,17 @@
 package com.example;
 
 import java.io.IOException;
+import java.time.LocalDate;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Priority;
+import javafx.scene.layout.VBox;
 
 public class SecondaryController {
 
@@ -22,9 +27,18 @@ public class SecondaryController {
     @FXML
     private HBox spacer;
 
+    
+    @FXML
+    private Button editButton;
+
     @FXML
     private Label nodeNameLabel;
 
+    @FXML
+    private Button readButton;
+
+    @FXML
+    private VBox screenVBox;
 
     public void changeInfos() {
         HBox.setHgrow(spacer, Priority.ALWAYS);
@@ -39,5 +53,40 @@ public class SecondaryController {
     void deco() throws IOException{
         User.deco();
         App.setRoot("primary");
+    }
+
+    @FXML
+    void goToEdit(ActionEvent event) {
+        nodeNameLabel.setText("Edition de la fiche de frais");
+        LocalDate date = LocalDate.now();
+
+        VBox box = new VBox();
+        Label etat_frais = new Label("Etat de frais engagés : mois de " + date.getMonth());
+        Label matricule = new Label("Matricule : " + User.getMATRICULE());
+        Label nom = new Label("Nom : " + User.getNOM() + " " + User.getPRENOM());
+
+        box.getChildren().add(etat_frais);
+        box.getChildren().add(matricule);
+        box.getChildren().add(nom);
+
+        box.setAlignment(Pos.TOP_CENTER);
+
+        screenVBox.getChildren().setAll(box);
+    }
+
+    @FXML
+    void goToHome(ActionEvent event){
+        HBox buttonAlign = new HBox(editButton, readButton);
+        HBox.setMargin(buttonAlign, new Insets(0,0,10,10));
+
+        screenVBox.getChildren().setAll(new Label(" Vos options :"), buttonAlign);
+        nodeNameLabel.setText("Accueil");
+    }
+
+    @FXML 
+    void goToRead(ActionEvent event){
+        nodeNameLabel.setText("Consulation des fiches de frais précédentes");
+        screenVBox.getChildren().setAll(new VBox());
+
     }
 }
