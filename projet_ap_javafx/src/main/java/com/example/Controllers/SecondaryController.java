831fc1cd00803d19e4826a3e86577b616d795f05
Thomas Dindin <thomas.dindin@gmail.com>
2023-02-19 20:26:53 +0100

    Affichage fonctionnel

--- projet_ap_javafx/src/main/java/com/example/Controllers/SecondaryController.java
+++ projet_ap_javafx/src/main/java/com/example/Controllers/SecondaryController.java
@@ -1,26 +1,12 @@
 package com.example.Controllers;
 
 import java.io.IOException;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-
 import com.example.App;
-import com.example.FraisForfaitaires;
 import com.example.User;
-import com.example.PartieSQL.Identification;
-
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.geometry.Insets;
-import javafx.geometry.Pos;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javafx.scene.control.Spinner;
-import javafx.scene.control.SpinnerValueFactory;
-import javafx.scene.layout.GridPane;
-import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 
 public class SecondaryController {
@@ -55,18 +41,6 @@ public class SecondaryController {
         username.setText(sexe + User.getNOM() + " " + User.getPRENOM() );
         type_agent.setText(type_agent_str);
     }
-
-
-    
-    private Spinner<Integer> createIntegerSpinner(int def, int min, int max){
-        Spinner<Integer> spinner = new Spinner<>();
-        SpinnerValueFactory<Integer> nuiteeSpinnerFactory = new SpinnerValueFactory.IntegerSpinnerValueFactory(min ,max, def );
-        
-        spinner.setEditable(true);
-        spinner.setValueFactory(nuiteeSpinnerFactory);
-
-        return spinner;
-    }
     
 
     @FXML
@@ -77,40 +51,9 @@ public class SecondaryController {
 
     @FXML
     void goToEdit(ActionEvent event) {
-        Identification conn = new Identification();
-        ResultSet res = conn.getFrais();
-
-        ObservableList<FraisForfaitaires> list = FXCollections.observableArrayList();
-        
-        //   LocalDate date = LocalDate.now();
-
-        VBox box = new VBox();
-        GridPane grid = new GridPane();
-
-        try {
-            while (res.next()){
-                String nomFrais = res.getString("name");
-                Double coutFrais= res.getDouble("cost");
-                list.add(new FraisForfaitaires(nomFrais, coutFrais));
-            }
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-
-        box.getChildren().addAll(new Label("Frais forfaitisés :"));
-        list.forEach(e-> box.getChildren().add(new Label(e.getNom())));
-        box.setPadding(new Insets(50));
-        box.setAlignment(Pos.TOP_CENTER);
-
+        VisiteurEditionFrais display = new VisiteurEditionFrais();
+        VBox box = display.createDisplay();
         screenVBox.getChildren().setAll(box);
-    }
-
-    @FXML
-    void goToHome(ActionEvent event){
-        HBox buttonAlign = new HBox(editButton, readButton);
-        HBox.setMargin(buttonAlign, new Insets(0,0,10,10));
-
-        screenVBox.getChildren().setAll(new Label(" Vos options :"), buttonAlign);
     }
 
     @FXML 
--- projet_ap_javafx/src/main/java/com/example/Controllers/VisiteurEditionFrais.java
+++ projet_ap_javafx/src/main/java/com/example/Controllers/VisiteurEditionFrais.java
@@ -1,5 +1,78 @@
 package com.example.Controllers;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import com.example.FraisForfaitaires;
+import com.example.PartieSQL.Identification;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.control.Label;
+import javafx.scene.control.Spinner;
+import javafx.scene.control.SpinnerValueFactory;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.Priority;
+import javafx.scene.layout.VBox;
+
 public class VisiteurEditionFrais {
+    private Identification conn;
+    private ObservableList<FraisForfaitaires> list = FXCollections.observableArrayList();;
+    
+
+    public VisiteurEditionFrais() {
+        this.conn = new Identification();
+        
+    }
+
+    private Spinner<Integer> createIntegerSpinner(int def, int min, int max){
+        Spinner<Integer> spinner = new Spinner<>();
+        SpinnerValueFactory<Integer> nuiteeSpinnerFactory = new SpinnerValueFactory.IntegerSpinnerValueFactory(min ,max, def );
+        
+        spinner.setEditable(true);
+        spinner.setValueFactory(nuiteeSpinnerFactory);
+
+        return spinner;
+    }
+
+    private void remplirList(ResultSet res){
+        try {
+            while (res.next()){
+                String nomFrais = res.getString("name");
+                Double coutFrais= res.getDouble("cost");
+                list.add(new FraisForfaitaires(nomFrais, coutFrais));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public VBox createDisplay(){
+        ResultSet res = this.conn.getFrais();
+        remplirList(res);
+
+        VBox box = new VBox();
+        GridPane grid = new GridPane();
+        grid.setHgap(20);
+        int rowCount = 0;
+
+        for (FraisForfaitaires i : list){
+            grid.add(new Label(i.getNom()), 0, rowCount);
+            grid.add(createIntegerSpinner(0, 0, 31), 1, rowCount);
+            grid.add(new Label(Double.toString(i.getMontantU())), 2, rowCount);
+            grid.add(new Label(Double.toString(i.getTotal())), 3, rowCount);
+
+            rowCount++;
+        }
+
+        box.getChildren().addAll(new Label("Frais forfaitisés :"), grid);
+        box.setPadding(new Insets(50));
+        VBox.setVgrow(box, Priority.ALWAYS);
+        box.setAlignment(Pos.TOP_CENTER);
+
+        return box;
+    }
 
 }
